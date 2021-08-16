22ee7275673415338db31dcbae21a9d5805f8cc0
garam918 <62510764+garam918@users.noreply.github.com>
2021-08-16 20:14:01 +0900

    Feat : Bottom Navigation Activity의 하단 메뉴에 talk, more 메뉴를 위한 Fragment 추가 & navigation에 추가

--- app/src/main/java/com/gr/assignment/ui/dashboard/DashboardViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.gr.assignment.ui.dashboard
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class DashboardViewModel : ViewModel() {
-
-}
