39bd646f27459486a325f00f9331014e662a8313
garam918 <62510764+garam918@users.noreply.github.com>
2021-08-20 19:58:25 +0900

    Feat : Retrofit Builder 정의

---
+++ app/src/main/java/com/gr/assignment/network/RetrofitBuilder.kt
@@ -0,0 +1,18 @@
+package com.gr.assignment.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitBuilder {
+
+    private const val baseUrl = "https://alia.dalbitsoft.com"
+
+
+    fun getRetrofit() : Retrofit {
+        return Retrofit.Builder().baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val networkService: NetworkService = getRetrofit().create(NetworkService::class.java)
+}
