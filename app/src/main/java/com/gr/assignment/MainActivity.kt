aef41681ef7292e97a4db02eea0856defd3fa646
garam918 <62510764+garam918@users.noreply.github.com>
2021-08-20 20:00:04 +0900

    Feat : 처음 실행하는 MainActivity 변경

--- app/src/main/java/com/gr/assignment/MainActivity.kt
+++ app/src/main/java/com/gr/assignment/MainActivity.kt
@@ -9,12 +9,22 @@ import com.gr.assignment.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var mainVieModel : MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main,)
-        binding.lifecycleOwner = this
+        init()
 
+    }
+
+    private fun init() {
+        mainVieModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.viewModel = mainVieModel
+
+        mainVieModel.getSchoolList()
     }
 }
