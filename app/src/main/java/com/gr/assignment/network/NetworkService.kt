54f246358c75547f30a31b36d4316c43ea989c09
garam918 <62510764+garam918@users.noreply.github.com>
2021-08-20 19:59:20 +0900

    Feat : Api 호출을 위한 Http Method 구현

--- app/src/main/java/com/gr/assignment/network/NetworkService.kt
+++ app/src/main/java/com/gr/assignment/network/NetworkService.kt
@@ -1,11 +1,64 @@
 package com.gr.assignment.network
 
-import com.gr.assignment.data.ResponseData
+import com.gr.assignment.data.*
 import retrofit2.Call
-import retrofit2.http.GET
+import retrofit2.http.*
 
 interface NetworkService {
 
     @GET("/coding_test.php")
     fun getLectureInfo() : Call<ResponseData>
+
+    @GET("/test/school_list")
+    fun getSchoolList() : Call<ResponseSchoolListData>
+
+    @GET("/test/get_school_info")
+    fun getSchoolDetail(
+        @Query("id") id : Int
+    ) : Call<ResponseSchoolDetailData>
+
+    @GET("/test/login")
+    fun login(
+        @Query("id") id : String,
+        @Query("pw") pw : String,
+        @Query("token") token : String
+    ) : Call<ResponseLoginData>
+
+    @GET("/test/course_list")
+    fun getCourseList(
+        @Header("Authorization") authorHeader : String,
+        @Query("token") token: String
+    ) : Call<ResponseCourseListData>
+
+    @GET("/test/course_contents")
+    fun getCourseContent(
+        @Query("course_id") courseId : Int,
+        @Query("token") token : String
+    ) : Call<ResponseCourseContentData>
+
+    @GET("test/board")
+    fun getBoard(
+        @Query("contents_id") contentsId : Int,
+        @Query("token") token: String
+    ) : Call<ResponseBoardData>
+
+    @POST("/test/board")
+    fun boardWrite(
+        @Body contents_id: Int,
+        @Body token: String,
+        @Body subject : String,
+        @Body content : String
+    ) : Call<ResponseBoardData>
+
+    @DELETE("/test/board")
+    fun deleteBoard(
+        @Body board_id : Int,
+        @Body token : String
+    ) : Call<ResponseBoardData>
+
+    @GET("/test/web")
+    fun getWebView(
+        @Header("token") token: String,
+        @Header("contentsid") contentsId: Int
+    ) : Call<ResponseBoardData>
 }
